1e17a99f0a1b72f4647b17d16b3f4007dfd6e482
Alfredo Granda <agranda@fandango.com>
2018-06-19 14:29:27 -0500

    Update buildsepc

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict';
 
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
+// const awsServerlessExpress = require('aws-serverless-express');
+// const app = require('./app');
+// const server = awsServerlessExpress.createServer(app);
+//
+// exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+const time = require('time');
+exports.handler = (event, context, callback) => {
+	const currentTime = new time.Date();
+	currentTime.setTimezone("America/Los_Angeles");
+	callback(null, {
+		statusCode: '200',
+		body: 'The time in Los Angeles is: ' + currentTime.toString(),
+	});
+};
