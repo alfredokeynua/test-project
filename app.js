627f5b0910d83aa190de1eee668dcfd4a33334ef
Alfredo Granda <agranda@fandango.com>
2018-06-19 09:05:35 -0500

    FirstSetup

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function(req, res) {
   res.send({
@@ -9,10 +9,10 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World POST!"
   });
 });
 
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
