80d810d0b47958cadf27a058835b6793292ea431
Alfredo Granda <agranda@fandango.com>
2018-06-26 09:12:57 -0500

    Update

--- code-build-extras.sh
+++ code-build-extras.sh
@@ -33,27 +33,6 @@ if [[ $CODEBUILD_SOURCE_VERSION == pr/* ]] ; then
   export BUILD_NAME="$BUILD_NAME-$PULL_REQUEST_ID"
 fi
 
-# Determine the deployable zip file name based on the branch
-# -------------------------------------------------------------
-export ZIP_FOR_DEV="dev.zip"
-export ZIP_FOR_PROD="master.zip"
-export ZIP_NAME=""
-if [ "$GIT_BRANCH" == "dev" ] ; then
-  export ZIP_NAME="dev.zip"
-fi
-if [ "$GIT_BRANCH" == "release-*" ] ; then
-  export ZIP_NAME="master.zip"
-fi
-if [ "$GIT_BRANCH" == "master" ] ; then
-  export ZIP_NAME="master.zip"
-fi
-
-# Clear the zip name when it is a pull request
-# -------------------------------------------------------------
-if [ "$IS_PULL_REQUEST" == "true" ] ; then
-  export ZIP_NAME=""
-fi
-
 # Echo the export commands to include in the build-spec.yml
 # -------------------------------------------------------------
 # In the buildspec.yml run:
@@ -71,4 +50,3 @@ echo "export GIT_MESSAGE=\"$GIT_MESSAGE\""
 echo "export GIT_TAG=\"$GIT_TAG\""
 echo "export GIT_PULL_REQUEST=\"$IS_PULL_REQUEST\""
 echo "export GIT_NAME=\"$BUILD_NAME\""
-echo "export ZIP_NAME=\"$ZIP_NAME\""
