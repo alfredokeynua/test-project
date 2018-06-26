ac6142682fcbc6ff34c53eb025fdc77b51450f26
Alfredo Granda <agranda@fandango.com>
2018-06-26 11:27:13 -0500

    Update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ exports.handler = (event, context, callback) => {
 			time: currentTime,
 			env: process.env ? process.env.THE_ENV_NAME : 'NO ENV',
 			sample: {
-				go: 'Deployments work!!',
+				go: 'Deployments work!! Automatically!!',
 				two: 'test',
 				thre: 'hello',
 				other: 'world',
